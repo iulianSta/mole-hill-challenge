5fe4ded2aa2a648ed067e8e282cda2eb1fe83820
Iulian Stan <iulian1226@gmail.com>
2022-01-04 13:52:59 +0100

    update answer.py

--- answer.py
+++ answer.py
@@ -1,3 +1,5 @@
+# A mountain of a mole hill
+
 S = 16
 answer = 0
 L = ' '* S
@@ -5,7 +7,7 @@ L = ' '* S
 for i in range(S):
     L, L0 = input(), L
     inside = False
-    for j,c in enumerate(L):
+    for j, c in enumerate(L):
         if c =='|' or (c =='+' and L0[j] in '|+'): inside = not inside
         elif c =='o' and inside: answer += 1
 print(answer)
